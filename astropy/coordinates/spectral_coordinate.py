cce0cf575316352f4c4c919b2df7ef6bf6c01c00
nmearl <contact@nicholasearl.me>
2019-08-02 14:12:26 -0400

    Implement ndarray-specific functionality for behaving like quantities

--- astropy/coordinates/spectral_coordinate.py
+++ astropy/coordinates/spectral_coordinate.py
@@ -1,8 +1,9 @@
 import astropy.units as u
 import numpy as np
-from astropy.coordinates import BaseCoordinateFrame
+from astropy.coordinates.baseframe import BaseCoordinateFrame
 import logging
 from astropy.constants import c
+from astropy.utils.compat import NUMPY_LT_1_14
 
 DOPPLER_CONVENTIONS = {
     'radio': u.doppler_radio,
@@ -17,9 +18,9 @@ class SpectralCoord(u.Quantity):
     """
     Coordinate object representing a spectral axis.
 
-    Parameters
+    Attributes
     ----------
-    data : ndarray or `Quantity`
+    value : ndarray or `Quantity` or `SpectralCoord`
         Spectral axis data values.
     unit : str or `Unit`
         Unit for the given data.
@@ -30,22 +31,28 @@ class SpectralCoord(u.Quantity):
     target : `BaseCoordinateFrame`
         The coordinate frame of the target.
     """
-    def __init__(self, data, unit=None, rest_value=None,
-                 velocity_convention=None, observer=None, target=None):
-        super().__init__(data, unit=unit)
+    def __new__(cls, value, unit=None, rest_value=None,
+                velocity_convention=None, observer=None, target=None,
+                **kwargs):
+        obj = super().__new__(cls, value, unit=unit, **kwargs)
 
-        self.rest_value = rest_value or u.Quantity(0, unit)
-        self.velocity_convention = DOPPLER_CONVENTIONS.get(
-            velocity_convention)
+        obj.rest_value = rest_value or 0 * unit
+        obj.velocity_convention = velocity_convention or 'relativistic'
+        obj.observer = observer
+        obj.target = target
 
-        if not issubclass(observer.__class__, BaseCoordinateFrame) or \
-            not issubclass(target.__class__, BaseCoordinateFrame):
+        return obj
 
-            raise ValueError("`{}` is not a subclass of `BaseCoordinateFrame`"
-                "".format(observer))
+    def __array_finalize__(self, obj):
+        super().__array_finalize__(obj)
 
-        self._observer = observer
-        self._target = target
+        if obj is not None:
+            unit = getattr(obj, 'unit', u.AA)
+            self.rest_value = getattr(obj, '_rest_value', 0 * unit)
+            self.velocity_convention = getattr(obj, '_velocity_convention',
+                                               'relativistic')
+            self.observer = getattr(obj, '_observer', None)
+            self.target = getattr(obj, '_target', None)
 
     def __quantity_subclass__(self, unit):
         """
@@ -56,51 +63,114 @@ class SpectralCoord(u.Quantity):
 
     @property
     def observer(self):
+        """
+        The coordinate frame from which the observation was taken.
+
+        Returns
+        -------
+        : `BaseCoordinateFrame`
+            The astropy coordinate frame representing the observation.
+        """
         return self._observer
+
+    @observer.setter
+    def observer(self, value):
+        if value is not None and not isinstance(value, BaseCoordinateFrame):
+            raise ValueError("`{}` is not a subclass of `BaseCoordinateFrame`"
+                             "".format(value))
+
+        self._observer = value
 
     @property
     def target(self):
+        """
+        The coordinate frame of the object being observed.
+
+        Returns
+        -------
+        : `BaseCoordinateFrame`
+            The astropy coordinate frame representing the target.
+        """
         return self._target
 
     @target.setter
     def target(self, value):
-        if not issubclass(value.__class__, BaseCoordinateFrame):
+        if value is not None and not isinstance(value, BaseCoordinateFrame):
             raise ValueError("`{}` is not a subclass of `BaseCoordinateFrame`"
-                "".format(value))
+                             "".format(value))
 
         self._target = value
 
     @property
     def rest_value(self):
+        """
+        The rest value of the spectrum used for transformations to/from
+        velocity space.
+
+        Returns
+        -------
+        : `Quantity`
+            Rest value as an astropy `Quantity` object.
+        """
         return self._rest_value
 
-    @property
+    @rest_value.setter
     @u.quantity_input(value=['length', 'frequency', 'energy', 'speed'])
     def rest_value(self, value):
         self._rest_value = value
 
     @property
     def velocity_convention(self):
+        """
+        The defined convention for conversions to/from velocity space.
+
+        Returns
+        -------
+        : str
+            One of 'optical', 'radio', or 'relativistic' representing the
+            equivalency used in the unit conversions.
+        """
         return self._velocity_convention
 
     @velocity_convention.setter
     def velocity_convention(self, value):
         if value not in DOPPLER_CONVENTIONS:
-            logging.warning("Unrecognized velocity convention: {}".format(
+            raise ValueError("Unrecognized velocity convention: {}".format(
                 value))
 
         self._velocity_convention = value
 
     def transform_frame(self, frame=None, target=None):
         """
-        Transform the frame of the observer.
+        Transform the frame of the observation.
+
+        Parameters
+        ----------
+        frame : `BaseCoordinateFrame`
+            The new observation frame.
+        target : :class:`astropy.coordinates.SkyCoord`
+            The :class:`astropy.coordinates.SkyCoord` object representing
+            the target of the observation.
+
+        Returns
+        -------
+        : `SpectralCoord`
+            The new coordinate object representing the spectral data
+            transformed to the new observer frame.
         """
         if target is not None:
             self.target = target
 
+        # If not velocities are defined on the target or observer frames,
+        # assume they have zero velocity in their frame.
+
+
         # Get the velocity differentials for each frame
         init_obs_vel = self.target.transform_to(self.observer).velocity
-        fin_obs_vel = self.target.transform_to(self.observer).velocity
+        fin_obs_vel = self.target.transform_to(frame).velocity
+
+        # Store the new frame as the current observer frame
+        self.observer = frame
 
         # Calculate the velocity shift between the two vectors
         obs_vel_shift = init_obs_vel - fin_obs_vel
@@ -109,8 +179,6 @@ class SpectralCoord(u.Quantity):
         # between the target and the new observation frame.
         delta_vel = np.dot(obs_vel_shift, fin_obs_vel) / np.linalg.norm(
             fin_obs_vel)
-
-        self._observer = frame
 
         # Apply the velocity shift to the stored spectral data
         new_data = (self.to('hz') * (1 + delta_vel / c.cgs)).to(self.unit)
@@ -125,22 +193,40 @@ class SpectralCoord(u.Quantity):
 
     def to(self, *args, rest=None, convention=None, **kwargs):
         """
-        Overload the parent ``to`` method to provide parameters for defining
+        Overloaded parent ``to`` method to provide parameters for defining
         rest value and pre-defined conventions for unit transformations.
+
+        Returns
+        -------
+        : `SpectralCoord`
+            New spectral coordinate object with data converted to the new unit.
         """
         if rest is not None:
             self.rest = rest
 
         if convention is not None:
-            if isinstance(convention, str):
-                convention = DOPPLER_CONVENTIONS.get(convention)
-
             self.velocity_convention = convention
 
-        kwargs.get('equivalencies', []).append(u.spectral() +
-            self.velocity_convention(self.rest_value))
+        vel_conv = DOPPLER_CONVENTIONS[self.velocity_convention]
+
+        # Compose the equivalencies for spectral conversions including the
+        # appropriate velocity handling.
+        kwargs.get('equivalencies', []).append(u.spectral() + vel_conv(
+            self.rest_value))
 
         return super().to(*args, **kwargs)
 
     def __repr__(self):
-        pass
+        prefixstr = '<' + self.__class__.__name__ + ' '
+        sep = ',' if NUMPY_LT_1_14 else ', '
+        arrstr = np.array2string(self.view(np.ndarray), separator=sep,
+                                 prefix=prefixstr)
+        obs_frame = self.observer.__class__.__name__ \
+            if self.observer is not None else 'None'
+        tar_frame = self.target.__class__.__name__ \
+            if self.target is not None else 'None'
+        return f'{prefixstr}{arrstr}{self._unitstr:s}, ' \
+            f'rest_value={self.rest_value}, ' \
+            f'velocity_convention={self.velocity_convention}, ' \
+            f'observer={obs_frame}, target={tar_frame}>'
+
